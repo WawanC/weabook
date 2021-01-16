a6e3972fdbfb55a65ac18d937039a7acf764ebf2
WawanC <kurniawancristianto@gmail.com>
2021-01-16 23:46:15 +0700

    Modify Feed Screen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -3,8 +3,36 @@ import 'package:flutter/material.dart';
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Feed Screen"),
+    return ListView.builder(
+      padding: EdgeInsets.all(10),
+      itemBuilder: (bctx, idx) {
+        return Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: ListTile(
+            onTap: () {},
+            contentPadding: EdgeInsets.all(10),
+            leading: Container(
+              width: 50,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            title: Text(
+              "Player post a review :",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text("Naruto Shipudden"),
+            trailing: Icon(Icons.arrow_forward_ios_sharp),
+          ),
+        );
+      },
+      itemCount: 4,
+      reverse: true,
     );
   }
 }
